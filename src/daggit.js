2b080871ab8f36a71bff2a62acfc3314681fd561
Nic Sandfield <niczero@wow.com>
2017-12-17 22:48:21 +0000

    Add themes :art:

--- src/daggit.js
+++ src/daggit.js
@@ -1,234 +1,272 @@
-var gitGraph = function (canvas, rawGraphList, config) {
-  if (!canvas.getContext) {
-    return;
-  }
-  
-  if (typeof config === 'undefined') {
-    config = {
-      unitSize: 16,
-      lineWidth: 4,
-      nodeRadius: 5
+(function () {
+
+  'use strict';
+
+  var self = {},
+      flows = [],
+      graphList = [];
+
+  function daggit (canvas, rawGraphList, config) {
+
+    if (!canvas.getContext) {
+      return;
+    }
+
+    if (typeof config === 'undefined') {
+      self.config = {
+        unitSize: 16,
+        lineWidth: 4,
+        nodeRadius: 5
+      };
+    }
+    else {
+      self.config = config;
+    }
+    self.theme = {
+      nodeInnerColour: '#FFFFFF',
+      nodeOuterColour: '#000000',
+      nodeShadowColour: 'rgba(0, 0, 0, 0.5)'
     };
-  }
-  var theme = {
-    nodeInnerColour: '#FFFFFF',
-    nodeOuterColour: '#000000',
-    nodeShadowColour: 'rgba(0, 0, 0, 0.5)'
-  };
-  
-  var flows = [];
-  var graphList = [];
-  
-  var ctx = canvas.getContext('2d');
-  
-  let devicePixelRatio = window.devicePixelRatio || 1,
-      backingStoreRatio = ctx.webkitBackingStorePixelRatio
-        || ctx.mozBackingStorePixelRatio
-        || ctx.msBackingStorePixelRatio
-        || ctx.oBackingStorePixelRatio
-        || ctx.backingStorePixelRatio
-        || 1;
+config.theme = 'dark';
+    if (typeof config.theme === 'undefined' || config.theme === 'light') {
+      self.config.colours = [ '0079BF', '026AA7', '055A8C', '094C72', '0C3953',
+        '61BD4F', '5AAC44', '519839', '49852E', '3F6F21', 'FFAB4A', 'E99E40',
+        'D29034', 'BB8129', 'A0711C', 'EB5A46', 'CF513D', 'B04632', '933B27',
+        '6E2F1A', 'F2D600', 'E6C60D', 'D9B51C', 'CCA42B', 'BD903C', 'C377E0',
+        'A86CC1', '89609E', '6C547B', '484553', 'FF80CE', 'E76EB1', 'CD5A91',
+        'B44772', '96304C', '00C2E0', '00AECC', '0098B7', '0082A0', '006988',
+        '51E898', '4FD683', '4DC26B', '4CAF54', '4A9839', '676D70', '333333',
+        '222222', '000000', '42548E', '3E4D80', '3A476F', '36405F', '30364C' ];
+      self.theme.nodeInnerColour = '#FFFFFF';
+      self.theme.nodeOuterColour = '#000000';
+    }
+    else {
+      self.config.colours = [ 'BCD9EA', '8BBDD9', '5BA4CF', '298FCA', '0079BF',
+        'D6ECD2', 'B7DDB0', '99D18F', '7BC86C', '61BD4F', 'FCE8D2', 'FAD8B0',
+        'FDC788', 'FFB968', 'FFAB4A', 'F5D3CE', 'EFB3AB', 'EC9488', 'EF7564',
+        'EB5A46', 'FAF3C0', 'F5EA92', 'F3E260', 'F5DD29', 'F2D600', 'EDDBF4',
+        'DFC0EB', 'D5A6E6', 'CD8DE5', 'C377E0', 'FCDEF0', 'FAC6E5', 'FFB0E1',
+        'FF95D6', 'FF80CE', 'BDECF3', '8FDFEB', '5DD3E5', '29CCE5', '00C2E0',
+        'D3F6E4', 'B3F1D0', '90ECC1', '6DECA9', '51E898', 'EDEFF0', 'E2E4E6',
+        'D6DADC', '838C91', '676D70', 'D2D7E5', 'B2B9D0', '838FB5', '6170A1',
+        '42548E' ];
+      self.theme.nodeInnerColour = '#000000';
+      self.theme.nodeOuterColour = '#FFFFFF';
+    }
 
-  var ratio = devicePixelRatio / backingStoreRatio;
+    let ctx = self.ctx = canvas.getContext('2d');
 
-  var init = function () {
+    let devicePixelRatio = window.devicePixelRatio || 1,
+        backingStoreRatio = ctx.webkitBackingStorePixelRatio
+          || ctx.mozBackingStorePixelRatio
+          || ctx.msBackingStorePixelRatio
+          || ctx.oBackingStorePixelRatio
+          || ctx.backingStorePixelRatio
+          || 1;
+
+    self.ratio = devicePixelRatio / backingStoreRatio;
+
     let maxWidth = 0;
-    
+
     for (let i = 0, len = rawGraphList.length; i < len; ++i) {
-      let midStr = rawGraphList[i].replace(/\s+/g, ' ').replace(/^\s+|\s+$/g, '');
-      
-      maxWidth = Math.max(midStr.replace(/(\_|\s)/g, '').length, maxWidth);
-      
+      let midStr = rawGraphList[i].replace(/^\s+|\s+$/g, '').replace(/\s\s+/g, ' ');
+
+      maxWidth = Math.max(midStr.replace(/(_|\s)/g, '').length, maxWidth);
+
       let row = midStr.split('');
-      
+
       graphList.unshift(row);
     }
-    
-    let width = maxWidth * config.unitSize;
-    let height = graphList.length * config.unitSize;
 
-    canvas.width = width * ratio;
-    canvas.height = height * ratio;
+    let w = maxWidth * self.config.unitSize,
+        h = graphList.length * self.config.unitSize;
 
-    canvas.style.width = width + 'px';
-    canvas.style.height = height + 'px';
-    
-    ctx.lineWidth = config.lineWidth;
+    canvas.width = w * self.ratio;
+    canvas.height = h * self.ratio;
+
+    canvas.style.width = w + 'px';
+    canvas.style.height = h + 'px';
+    self.canvas = canvas;
+
+    ctx.lineWidth = self.config.lineWidth;
     ctx.lineJoin = 'round';
     ctx.lineCap = 'round';
 
-    ctx.scale(ratio, ratio);
-  };
-  
-  var genRandomStr = function () {
-    let rnum = Math.floor(Math.random() * colours.length);
-    return colours[rnum];
-  };
-  
-  var findFlow = function (id) {
+    ctx.scale(self.ratio, self.ratio);
+
+    draw(self, graphList);
+  }
+
+  function genRandomStr () {
+    let rnum = Math.floor(Math.random() * self.config.colours.length);
+    return self.config.colours[rnum];
+  }
+
+  function findFlow (id) {
     let i = flows.length;
-    
+
     while (i-- && flows[i].id !== id) {}
-    
+
     return i;
-  };
-  
-  var findColumn = function (symbol, row) {
+  }
+
+  function findColumn (symbol, row) {
     let i = row.length;
-    
+
     while (i-- && row[i] !== symbol) {}
-    
+
     return i;
-  };
-  
-  var findBranchOut = function (row) {
+  }
+
+  function findBranchOut (row) {
     if (!row) {
       return -1
     }
-    
+
     let i = row.length;
-    
+
     while (i--
       && !(row[i - 1] && row[i] === '/' && row[i - 1] === '|')
       && !(row[i - 2] && row[i] === '_' && row[i - 2] === '|')) {}
-    
+
     return i;
   }
-  
-  var genNewFlow = function () {
+
+  function genNewFlow () {
     let newId;
-    
+
     do {
       newId = genRandomStr();
     } while (findFlow(newId) !== -1);
-    
+
     return {id:newId, color:'#' + newId};
-  };
-  
+  }
+
   // draw method
-  var drawLineRight = function (x, y, color) {
+  function drawLineRight (self, x, y, color) {
+    let ctx = self.ctx;
     ctx.strokeStyle = color;
     ctx.beginPath();
-    ctx.moveTo(x,                   y + config.unitSize / 2);
-    ctx.lineTo(x + config.unitSize, y + config.unitSize / 2);
+    ctx.moveTo(x,                        y + self.config.unitSize / 2);
+    ctx.lineTo(x + self.config.unitSize, y + self.config.unitSize / 2);
     ctx.stroke();
-  };
-  
-  var drawLineUp = function (x, y, color) {
+  }
+
+  function drawLineUp (self, x, y, color) {
+    let ctx = self.ctx;
     ctx.strokeStyle = color;
     ctx.beginPath();
-    ctx.moveTo(x, y + config.unitSize / 2);
-    ctx.lineTo(x, y - config.unitSize / 2);
+    ctx.moveTo(x, y + self.config.unitSize / 2);
+    ctx.lineTo(x, y - self.config.unitSize / 2);
     ctx.stroke();
-  };
-  
-  var drawNode = function (x, y, color) {
+  }
+
+  function drawNode (self, x, y, color) {
+    let ctx = self.ctx;
     ctx.strokeStyle = color;
-    
-    drawLineUp(x, y, color);
-    
+
+    drawLineUp(self, x, y, color);
+
     ctx.save();
     ctx.beginPath();
-    ctx.fillStyle = theme.nodeOuterColour;
-    ctx.arc(x, y, config.nodeRadius, 0, Math.PI * 2, true);
-    ctx.shadowOffsetX = config.nodeRadius / 2;
-    ctx.shadowBlur = config.nodeRadius;
-    ctx.shadowColor = theme.nodeShadowColour;
+    ctx.fillStyle = self.theme.nodeOuterColour;
+    ctx.arc(x, y, self.config.nodeRadius, 0, Math.PI * 2, true);
+    ctx.shadowOffsetX = self.config.nodeRadius / 2;
+    ctx.shadowBlur = self.config.nodeRadius;
+    ctx.shadowColor = self.theme.nodeShadowColour;
     ctx.fill();
     ctx.beginPath();
-    ctx.fillStyle = theme.nodeInnerColour;
-    ctx.arc(x, y, config.nodeRadius / 2, 0, Math.PI * 2, true);
+    ctx.fillStyle = self.theme.nodeInnerColour;
+    ctx.arc(x, y, self.config.nodeRadius / 2, 0, Math.PI * 2, true);
     ctx.fill();
     ctx.restore();
-  };
-  
-  var drawLineIn = function (x, y, color) {
+  }
+
+  function drawLineIn (self, x, y, color) {
+    let ctx = self.ctx;
     ctx.strokeStyle = color;
-    
+
     ctx.beginPath();
-    ctx.moveTo(x + config.unitSize, y + config.unitSize / 2);
+    ctx.moveTo(x + self.config.unitSize, y + self.config.unitSize / 2);
     ctx.bezierCurveTo(
-      x + config.unitSize, y,
-      x,                   y,
-      x,                   y - config.unitSize / 2
+      x + self.config.unitSize, y,
+      x,                        y,
+      x,                        y - self.config.unitSize / 2
     );
     ctx.stroke();
-  };
-  
-  var drawLineOut = function (x, y, color) {
+  }
+
+  function drawLineOut (self, x, y, color) {
+    let ctx = self.ctx;
     ctx.strokeStyle = color;
     ctx.beginPath();
-    ctx.moveTo(x, y + config.unitSize / 2);
+    ctx.moveTo(x, y + self.config.unitSize / 2);
     ctx.bezierCurveTo(
-      x,                   y,
-      x + config.unitSize, y,
-      x + config.unitSize, y - config.unitSize / 2
+      x,                        y,
+      x + self.config.unitSize, y,
+      x + self.config.unitSize, y - self.config.unitSize / 2
     );
     ctx.stroke();
-  };
-  
-  var draw = function (graphList) {
-    
+  }
+
+  function draw (self, graphList) {
+
     // initiate for first row
     for (let i = 0, len = graphList[0].length; i < len; ++i) {
       if (graphList[0][i] !== '_' && graphList[0][i] !== ' ') {
         flows.push(genNewFlow());
       }
     }
-    
-    let y = (canvas.height / ratio) - config.unitSize / 2;
-    
+
+    let y = (self.canvas.height / self.ratio) - self.config.unitSize / 2;
+
     // iterate
     let prevRowLength = 0,
         condensePrevLength = 0,
         inlineIntersect = false;
     for (let i = 0, len = graphList.length; i < len; ++i) {
-      let x = config.unitSize / 2;
-      
-      currentRow = graphList[i];
-      nextRow = graphList[i + 1];
-      prevRow = graphList[i - 1];
-      
-      let flowSwapPos = -1;
-      
+      let x = self.config.unitSize / 2,
+          currentRow = graphList[i],
+          nextRow = graphList[i + 1],
+          prevRow = graphList[i - 1],
+          flowSwapPos = -1;
+
       let condenseCurrentLength = currentRow.filter(function (val) {
         return (val !== ' ' && val !== '_')
       }).length;
-      
+
       // pre process begin
       // use last row for analysing
       if (prevRow) {
         if (!inlineIntersect) {
           // intersect might happen
           for (let columnIndex = 0; columnIndex < prevRowLength; ++columnIndex) {
-            if (prevRow[columnIndex + 1] && prevRow[columnIndex] === '/'
-                && prevRow[columnIndex + 1] === '|'
-              || prevRow[columnIndex] === '_' && prevRow[columnIndex + 1] === '|'
-                && prevRow[columnIndex + 2] === '/') {
+            if (prevRow[columnIndex + 1] && prevRow[columnIndex] === '/' && prevRow[columnIndex + 1] === '|'
+                || prevRow[columnIndex] === '_' && prevRow[columnIndex + 1] === '|' && prevRow[columnIndex + 2] === '/') {
               flowSwapPos = columnIndex;
-              
+
               // swap two flows
               let tempFlow = {id:flows[flowSwapPos].id, color:flows[flowSwapPos].color};
-              
+
               flows[flowSwapPos].id = flows[flowSwapPos + 1].id;
               flows[flowSwapPos].color = flows[flowSwapPos + 1].color;
-              
+
               flows[flowSwapPos + 1].id = tempFlow.id;
               flows[flowSwapPos + 1].color = tempFlow.color;
             }
           }
         }
-        
+
         let nodePos;
         if (condensePrevLength < condenseCurrentLength
             && (nodePos = findColumn('*', currentRow)) !== -1
             && findColumn('_', currentRow) === -1) {
           flows.splice(nodePos - 1, 0, genNewFlow());
         }
-        
+
         if (prevRowLength > currentRow.length
             && (nodePos = findColumn('*', prevRow)) !== -1) {
-          
+
           if (findColumn('_', currentRow) === -1
               && findColumn('/', currentRow) === -1
               && findColumn('\\', currentRow) === -1) {
@@ -236,28 +274,28 @@ var gitGraph = function (canvas, rawGraphList, config) {
           }
         }
       } // done with the previous row
-      
+
       prevRowLength = currentRow.length;  // store for next round
       let condenseIndex = 0;
       condensePrevLength = 0;
       for (let columnIndex = 0; columnIndex < currentRow.length; ++columnIndex) {
         let column = currentRow[columnIndex];
-        
+
         if (column !== ' ' && column !== '_') {
           ++condensePrevLength;
         }
-        
+
         if (column === ' '
             && currentRow[columnIndex + 1]
             && currentRow[columnIndex + 1] === '_'
             && currentRow[columnIndex - 1]
             && currentRow[columnIndex - 1] === '|') {
           currentRow.splice(columnIndex, 1);
-          
+
           currentRow[columnIndex] = '/';
           column = '/';
         }
-        
+
         // create new flow only when no intersetc happened
         if (flowSwapPos === -1
             && column === '/'
@@ -265,7 +303,7 @@ var gitGraph = function (canvas, rawGraphList, config) {
             && currentRow[columnIndex - 1] === '|') {
           flows.splice(condenseIndex, 0, genNewFlow());
         }
-        
+
         // change \ and / to | when it's in the last position of the whole row
         if (column === '/' || column === '\\') {
           if (!(column === '/' && findBranchOut(nextRow) === -1)) {
@@ -273,206 +311,113 @@ var gitGraph = function (canvas, rawGraphList, config) {
             if ((lastLinePos = Math.max(findColumn('|', currentRow), findColumn('*', currentRow)) ) !== -1
                 && lastLinePos < columnIndex - 1) {
               while (currentRow[++lastLinePos] === ' ') {}
-              
+
               if (lastLinePos === columnIndex) {
                 currentRow[columnIndex] = '|';
               }
             }
           }
         }
-        
+
         if (column === '*'
             && prevRow
             && prevRow[condenseIndex + 1] === '\\') {
           flows.splice(condenseIndex + 1, 1);
         }
-        
+
         if (column !== ' ') {
           ++condenseIndex;
         }
       }
-      
+
       condenseCurrentLength = currentRow.filter(function (val) {
         return (val !== ' ' && val !== '_')
       }).length;
-      
+
       // do some clean up
       if (flows.length > condenseCurrentLength) {
         flows.splice(condenseCurrentLength, flows.length - condenseCurrentLength);
       }
-      
+
       // a little inline analysis and draw process
       for (let columnIndex = 0; columnIndex < currentRow.length; ) {
         let column = currentRow[columnIndex],
             prevColumn = currentRow[columnIndex - 1];
-        
+
         if (currentRow[columnIndex] === ' ') {
           currentRow.splice(columnIndex, 1);
-          x += config.unitSize;
-          
+          x += self.config.unitSize;
+
           continue;
         }
-        
+
         // inline intersect
         if ((column === '_' || column === '/')
             && currentRow[columnIndex - 1] === '|'
             && currentRow[columnIndex - 2] === '_') {
           inlineIntersect = true;
-          
+
           let tempFlow = flows.splice(columnIndex - 2, 1)[0];
           flows.splice(columnIndex - 1, 0, tempFlow);
           currentRow.splice(columnIndex - 2, 1);
-          
+
           columnIndex = columnIndex - 1;
         }
         else {
           inlineIntersect = false;
         }
-        
+
         let color = flows[columnIndex].color;
-        
+
         switch (column) {
           case '_' :
-            drawLineRight(x, y, color);
-            
-            x += config.unitSize;
+            drawLineRight(self, x, y, color);
+
+            x += self.config.unitSize;
             break;
-            
+
           case '*' :
-            drawNode(x, y, color);
+            drawNode(self, x, y, color);
             break;
-            
+
           case '|' :
-            drawLineUp(x, y, color);
+            drawLineUp(self, x, y, color);
             break;
-            
+
           case '/' :
             if (prevColumn
                 && (prevColumn === '/' || prevColumn === ' ')) {
-              x -= config.unitSize;
+              x -= self.config.unitSize;
             }
-            
-            drawLineOut(x, y, color);
-            
-            x += config.unitSize;
+
+            drawLineOut(self, x, y, color);
+
+            x += self.config.unitSize;
             break;
-            
+
           case '\\' :
-            drawLineIn(x, y, color);
+            drawLineIn(self, x, y, color);
             break;
         }
-        
+
         ++columnIndex;
       }
-      
-      y -= config.unitSize;
-    }
-  };
-  
-  var colours = [
-    "BCD9EA",
-    "8BBDD9",
-    "5BA4CF",
-    "298FCA",
-    "0079BF",
-    "026AA7",
-    "055A8C",
-    "094C72",
-    "0C3953",
-    "D6ECD2",
-    "B7DDB0",
-    "99D18F",
-    "7BC86C",
-    "61BD4F",
-    "5AAC44",
-    "519839",
-    "49852E",
-    "3F6F21",
-    "FCE8D2",
-    "FAD8B0",
-    "FDC788",
-    "FFB968",
-    "FFAB4A",
-    "E99E40",
-    "D29034",
-    "BB8129",
-    "A0711C",
-    "F5D3CE",
-    "EFB3AB",
-    "EC9488",
-    "EF7564",
-    "EB5A46",
-    "CF513D",
-    "B04632",
-    "933B27",
-    "6E2F1A",
-    "FAF3C0",
-    "F5EA92",
-    "F3E260",
-    "F5DD29",
-    "F2D600",
-    "E6C60D",
-    "D9B51C",
-    "CCA42B",
-    "BD903C",
-    "DFC0EB",
-    "D5A6E6",
-    "CD8DE5",
-    "C377E0",
-    "A86CC1",
-    "89609E",
-    "6C547B",
-    "484553",
-    "FCDEF0",
-    "FAC6E5",
-    "FFB0E1",
-    "FF95D6",
-    "FF80CE",
-    "E76EB1",
-    "CD5A91",
-    "B44772",
-    "96304C",
-    "BDECF3",
-    "8FDFEB",
-    "5DD3E5",
-    "29CCE5",
-    "00C2E0",
-    "00AECC",
-    "0098B7",
-    "0082A0",
-    "006988",
-    "D3F6E4",
-    "B3F1D0",
-    "90ECC1",
-    "6DECA9",
-    "51E898",
-    "4FD683",
-    "4DC26B",
-    "4CAF54",
-    "4A9839",
-    "FFFFFF",
-    "EDEFF0",
-    "E2E4E6",
-    "D6DADC",
-    "838C91",
-    "676D70",
-    "333333",
-    "222222",
-    "000000",
-    "D2D7E5",
-    "B2B9D0",
-    "838FB5",
-    "6170A1",
-    "42548E",
-    "3E4D80",
-    "3A476F",
-    "36405F",
-    "30364C"
-  ];
 
-  init();
-  draw(graphList);
-};
+      y -= self.config.unitSize;
+    }
+  }
+
+  if (typeof exports !== 'undefined' && typeof process !== 'undefined') {
+    // Node.js module.
+    module.exports = exports = daggit;
+  }
+  else if (typeof window === 'object') {
+    // Browser loading.
+    window.daggit = daggit;
+  }
+
+})();
+
 /*
  * Copyright (c) 2011--12, Terrence Lee <kill889@gmail.com>
  * Copyright (c) 2017, Nic Sandfield <niczero@wow.com>
